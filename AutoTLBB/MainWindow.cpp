7185d7761c34d6ed41c029418398ae9bd765165d
Hai Le <levanhai@u-shop.jp>
2018-01-09 15:06:44 +0900

    Add Qt Project

---
+++ AutoTLBB/MainWindow.cpp
@@ -0,0 +1,16 @@
+#include "MainWindow.hpp"
+#include "ui_MainWindow.h"
+
+#include <Windows.h>
+
+MainWindow::MainWindow(QWidget *parent) :
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
